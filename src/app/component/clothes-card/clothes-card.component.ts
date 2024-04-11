24b027b85a979d07a50f2d5615b83278eee14f1f
Amanda Márcia <111714791+AmandaJGS@users.noreply.github.com>
2024-04-11 17:08:34 -0300

    Add files via upload

---
+++ src/app/component/clothes-card/clothes-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clothes-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './clothes-card.component.html',
+  styleUrl: './clothes-card.component.css'
+})
+export class ClothesCardComponent {
+
+}
---
+++ src/app/interface/clothes.ts
@@ -0,0 +1,10 @@
+export interface Clothes {
+    id: number;
+    name: string;
+    marca: string;
+    tamanho: string;
+    cor: string;
+    categoria: string;
+    descrição: string;
+    imagem: string;
+}
